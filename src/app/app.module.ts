7fe9df79aacbcc5d29cfa9b489310405d6d577b1
Max Elander <max@maxelander.se>
2022-06-27 10:30:16 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
+import { ScienceComponent } from './science/science.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntertainmentComponent,
+    ScienceComponent
   ],
   imports: [
     BrowserModule,
